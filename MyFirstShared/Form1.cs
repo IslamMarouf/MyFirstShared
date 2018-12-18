0bde2dbc6e35d9e609c08b18eff18fcdd31041a9
soli <soli@DESKTOP-D79C7P1>
2018-12-18 11:18:17 +0200

    comments

--- MyFirstShared/Form1.cs
+++ MyFirstShared/Form1.cs
@@ -10,17 +10,20 @@ namespace MyFirstShared
             InitializeComponent();
         }
 
+        // this button will close 
         private void button1_Click(object sender, EventArgs e)
         {
             Close();
         }
 
+        // this button will display a text box.
         private void button2_Click(object sender, EventArgs e)
         {
             MessageBox.Show("This is for test porpuse!");
             
         }
 
+        // this button will show a new form
         private void button3_Click(object sender, EventArgs e)
         {
             SecondForm sform = new SecondForm();
