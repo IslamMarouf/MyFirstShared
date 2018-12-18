b9c6304440ec267b2ff1ab58c564f0ebc7d20d56
soli <soli@DESKTOP-D79C7P1>
2018-12-18 10:26:15 +0200

    just edit

--- MyFirstShared/Form1.cs
+++ MyFirstShared/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MyFirstShared
@@ -24,7 +17,7 @@ namespace MyFirstShared
 
         private void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("This is for test!");
+            MessageBox.Show("This is for test porpuse!");
             
         }
     }
